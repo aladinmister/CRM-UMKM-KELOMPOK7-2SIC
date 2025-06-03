f107436c3c9be38d8a62cd3a9a23298ea47f80a9
Wahyu <tri23si@mahasiswa.pcr.ac.id>
2025-06-03 12:41:06 +0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import './App.css'
 import SalesManagement from './components/Pages/SalesManagement'
 import ProdukManagement from './components/Pages/ProdukManagement'
 import CustomerManagement from './components/Pages/CustomerManagement'
+import Laporan from './components/Pages/Laporan'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +20,7 @@ function App() {
     <Route path= "/penjualan" element = {<SalesManagement/>} />
     <Route path= "/produk" element = {<ProdukManagement/>} />
     <Route path= "/pelanggan" element = {<CustomerManagement/>} />
+    <Route path= "/laporan" element = {<Laporan/>} />
 
     
     </Route>
